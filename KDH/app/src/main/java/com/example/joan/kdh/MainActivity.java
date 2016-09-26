0b79841764fccb2c58f9db896fc8f374844d8e21
Gruci <skeksksk@gmail.com>
2016-09-26 15:47:54 +0900

    16/09/26

--- KDH/app/src/main/java/com/example/joan/kdh/MainActivity.java
+++ KDH/app/src/main/java/com/example/joan/kdh/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.joan.kdh;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        TextView myText = new TextView(this);
+        myText.setText("java를 활용한 레이아웃 구성");
+        setContentView(myText);
+        //setContentView(R.layout.activity_main);
     }
 }
